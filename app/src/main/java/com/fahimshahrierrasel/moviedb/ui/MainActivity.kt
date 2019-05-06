5bbc18a750d6a17df2848f693addb80bf0cc2ada
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-05-07 04:20:54 +0600

    MainActivity moved to ui package

--- app/src/main/java/com/fahimshahrierrasel/moviedb/ui/MainActivity.kt
+++ app/src/main/java/com/fahimshahrierrasel/moviedb/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fahimshahrierrasel.moviedb
+package com.fahimshahrierrasel.moviedb.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fahimshahrierrasel.moviedb.R
 
 class MainActivity : AppCompatActivity() {
 
