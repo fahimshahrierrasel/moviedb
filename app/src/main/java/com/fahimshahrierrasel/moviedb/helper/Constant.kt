af18fca854e60efdb9c781735a6a626fde92d83c
Md. Fahim Shahrier Rasel <fahimshahrierrasel@users.noreply.github.com>
2019-07-08 22:26:32 +0600

    Update Constant.kt

--- app/src/main/java/com/fahimshahrierrasel/moviedb/helper/Constant.kt
+++ app/src/main/java/com/fahimshahrierrasel/moviedb/helper/Constant.kt
@@ -1,7 +1,7 @@
 package com.fahimshahrierrasel.moviedb.helper
 
 const val API_BASE_URL = "https://api.themoviedb.org/3/"
-const val apiKey = "6d166c524405fe75234d2f5aaf4b2f90"
+const val apiKey = "Your API Key"
 
 const val posterPrefix = "https://image.tmdb.org/t/p/w154"
 const val backdropPrefix = "https://image.tmdb.org/t/p/w780"
